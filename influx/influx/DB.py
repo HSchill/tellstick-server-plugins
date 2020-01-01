4d790b55d32a78ed5c03569fae6ce3b8fd674d46
Hans Schillstrom <hans.schillstrom@magseis.com>
2020-01-01 20:02:41 +0100

    InfuxDB sender added
    This plugin will make it possible to send sensor values to
    an Influx DB from a lua script, and display it with grafana.
    
    Signed-off-by: Hans Schillstrom <hans.schillstrom@magseis.com>

---
+++ influx/influx/DB.py
@@ -0,0 +1,72 @@
+﻿# -*- coding: utf-8 -*-
+
+from base import configuration, ConfigurationString, Plugin, implements
+import httplib, urllib
+
+__name__ = 'influx'
+
+
+@configuration(
+	influxServer = ConfigurationString(
+		defaultValue='',
+		title='InfluxDB server name',
+		description='Address to the database server',
+		minLength=4
+	),
+	port = ConfigurationString(
+		defaultValue='8086',
+		title='InfluxDB server port',
+		minLength=2,
+		maxLength=5
+	),
+	database = ConfigurationString(
+		defaultValue='tellstick',
+		title='Database name',
+		description='name of the database (create database <name>)',
+		minLength=4
+	),
+	username = ConfigurationString(
+		defaultValue='',
+		title='Username',
+		description='Leave blank if not needed'
+	),
+	password = ConfigurationString(
+		defaultValue='',
+		title='Password',
+		description='Leave blank if not needed'
+	)
+)
+
+class DB(Plugin):
+	def acode(self, src):
+		if isinstance(src, (str, bytearray)):
+			return src.replace(' ', '_').decode('utf-8','replace').encode('ascii','replace')
+		elif isinstance(src, unicode):
+			return src.replace(' ', '_').encode('ascii','replace')
+		else:
+			return src
+
+	def send(self, measurment, location, devname, devid, data, datatype, scale='0'):
+
+		params = "%s,location=%s,id=%s,name=%s,scale=%s,type=%s data=%s" % (self.acode(measurment), self.acode(location), self.acode(devid), self.acode(devname), self.acode(scale), self.acode(datatype), self.acode(data))
+
+		headers = { "Content-type" : "application/x-www-form-urlencoded",
+			"Accept"       : "*/*",
+			"Host"         : "%s" % self.config('influxServer') }
+
+		urlstr = "/write?db=%s" % self.config('database')
+
+		if self.config('username') != '':
+			s = '&'
+			pu = {'u':'%s' % self.config('username')}
+			if self.config('password') != '':
+				pu.update({'p':'%s' % self.config('password')})
+			urlstr = s.join((urlstr, urllib.urlencode(pu)))
+
+		con = httplib.HTTPConnection(self.config('influxServer'), self.config('port'), timeout=10)
+		con.request("POST", urlstr, params, headers)
+		resp = con.getresponse()
+		data = resp.read()
+		con.close()
+		return data
+
---
+++ influx/setup.py
@@ -0,0 +1,39 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+setup(
+	name='Influx',
+	version='1.0',
+	icon='influx.png',
+	author='Hans Schillstrom',
+	author_email='hans@schillstrom.com',
+	category='notifications',
+	color='#000000',
+	description='Send sensor values from lua scripts to influx database',
+	long_description="""Use this plugin to add support for sending values to influxdb through Lua scripts.
+You need to supply your own influx-server and run create databaase name.
+
+Example to send an insert from Lua:
+```lua
+local ifdb = require "influx.DB"
+ifdb:send{
+        measurment='heating',
+        location='indoor',
+        devname=device:name(),
+        devid=device:id(),
+        value=Value,
+        scale=Scale
+}
+```
+""",
+	packages=['influx'],
+	entry_points={ \
+		'telldus.plugins': ['c = influx:DB [cREQ]']
+	},
+	extras_require = dict(cREQ = 'Base>=0.1\nTelldus>=0.1'),
+)
